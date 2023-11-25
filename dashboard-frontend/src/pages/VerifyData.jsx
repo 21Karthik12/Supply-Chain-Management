4ccaef0eb4669af3f85038a961172fa94ade6626
21Karthik12 <21.karthik.12.02@gmail.com>
2023-11-25 12:10:02 +0530

    stuff

--- dashboard-frontend/src/pages/VerifyData.jsx
+++ dashboard-frontend/src/pages/VerifyData.jsx
@@ -1,11 +1,18 @@
-import { HStack, VStack, Heading, Box, Flex, Input, Select, Button, FormLabel } from "@chakra-ui/react"
+import { HStack, VStack, Heading, Box, Flex, Text, Input, Table, Thead, Tr, Th, Tbody, Td, Select, Button, FormLabel } from "@chakra-ui/react"
 import { useState } from "react";
+import axios from 'axios'
 
 const VerifyData = () => {
     // useState variables to hold the values of the form fields
     const [sensorId, setSensorId] = useState(0);
     const [moduleId, setModuleId] = useState(0);
     const [moduleType, setModuleType] = useState('');
+    const [hashArr, setHashArr] = useState([])
+    const [mongoData, setMongoData] = useState([])
+    const [hyperData, setHyperData] = useState([])
+    const [message, setMessage] = useState("Is your sensor integrity in place?")
+
+
 
     const handleSubmit = async () => {
         // Perform actions with the form values (e.g., submit to a server)
@@ -13,8 +20,62 @@ const VerifyData = () => {
         console.log('Integer Field 2:', moduleId);
         console.log('Dropdown Value:', moduleType);
 
+        let url = `${import.meta.env.VITE_BASE_URL}:3001/getDataFromMongo`
 
+        let data = {
+            'sensorId': sensorId,
+            'moduleId': moduleId,
+            'module' : moduleType,
+        }
 
+        const getObjectsByHashes = (objects, hashes) => {
+            return  objects.filter(obj => hashes.includes(obj.Hash));
+            };
+
+        const headers = {
+            'Content-Type': 'application/json',
+            // Add any other headers as needed
+          };
+          
+          setMessage("Fetching data from MongoDB Cloud...")
+          // Making a POST request with headers
+          axios.post(url, data, { headers })
+            .then(response => {
+              console.log('MONGO:', response.data);
+              setMongoData(response.data)
+              let tempMongo = mongoData
+              tempMongo.forEach(obj => {
+                obj.mongo = true
+                obj.hyper = false
+                obj.integrity = false
+              })
+              setMongoData(tempMongo)
+              console.log("stuff added: ", mongoData)
+              const hashes = response.data.map(sensor => sensor.dataHash)
+              setHashArr(hashes)
+
+             axios.get(`${import.meta.env.VITE_BASE_URL}:3001/getDataFromFabric`).then((res) => {
+                console.log("FABRIC: ", res.data)
+                setHyperData(res.data)
+                setMessage("Fetching data from Hyperledger for verification...")
+
+                // Filter is based on sensorID
+                const filteredObjects = getObjectsByHashes(res.data, hashArr);
+                console.log("Filtered", filteredObjects)
+                setHyperData(filteredObjects)
+            })
+
+            })
+            .catch(error => {
+              console.error('Error:', error);
+            });
+
+            
+            // Function to fetch objects based on hash values
+            
+            
+           
+            
 
     };
     return (
@@ -69,7 +130,44 @@ const VerifyData = () => {
                                 </Flex>
                             </form>
                         </Box>
-                    </Flex>                </Box>
+                    </Flex>                
+                    </Box>
+                    <Box> 
+                        <Box border={"1px solid gray"} m={"1.5rem"} p={"1rem"}>
+                            <Text fontWeight={"500"} color={"gray"} fontSize={"0.9rem"}> {message} </Text>
+                        </Box>
+                    <Table variant="simple" borderWidth="1px" borderRadius="md" overflow="hidden">
+                        <Thead>
+                            <Tr>
+                            <Th textAlign="center">Timestamp</Th>
+                            {/* <Th textAlign="center">Sensor ID</Th> */}
+                            {/* <Th textAlign="center">Sensor Type</Th> */}
+                            {/* <Th textAlign="center">Module ID</Th> */}
+                            <Th textAlign="center">Sensor Reading</Th>
+                            <Th textAlign="center">Measurement Unit</Th>
+                            <Th textAlign="center">MongoDB Status</Th>
+                            <Th textAlign="center">Hyperledger Status</Th>
+                            <Th textAlign="center"> Integrity Check </Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            {mongoData.map((item, index) => (
+                            <Tr key={index}>
+                                <Td textAlign="center">{item.timestamp}</Td>
+                                {/* <Td textAlign="center">{item.sensorId}</Td> */}
+                                {/* <Td textAlign="center">{item.sensorType}</Td> */}
+                                {/* <Td textAlign="center">{item.moduleId}</Td> */}
+                                <Td textAlign="center">{item.value}</Td>
+                                <Td textAlign="center">{item.unit}</Td>
+                                <Td textAlign="center">{item.mongo}</Td>
+                                <Td textAlign="center">{item.hyper}</Td>
+                                <Td textAlign={"center"}>{item.integrity} </Td>
+
+                            </Tr>
+                            ))}
+                        </Tbody>
+                        </Table>
+                    </Box>
 
             </VStack>
         </>

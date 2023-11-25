1f5c743bf2f6f65a093317435730df04bd9e188d
roshan <roshnn74@gmail.com>
2023-11-25 11:45:50 +0530

    changes

--- dashboard-frontend/src/pages/LandingPage.jsx
+++ dashboard-frontend/src/pages/LandingPage.jsx
@@ -8,6 +8,7 @@ import ForecastTable from '../components/ForecastTable';
 import { useParams } from 'react-router-dom'
 import { IoMdArrowRoundBack } from 'react-icons/io';
 import io from 'socket.io-client'
+import Mod_Fleet from './Mod_Fleet';
 
 
 const routeDict = {
@@ -171,7 +172,7 @@ const LandingPage = () => {
           module_id == 4 && <RfidTable data={rfidData} />
         }
         {
-          module_id == 1 && <ModFleet />
+          module_id == 1 && <Mod_Fleet />
         }
       </VStack>
     </HStack>
--- dashboard-frontend/src/pages/Mod_Fleet.jsx
+++ dashboard-frontend/src/pages/Mod_Fleet.jsx
@@ -1,38 +1,38 @@
 import React from 'react';
+import { useState } from 'react';
 import io from 'socket.io-client'
 
 const socket = io(`${import.meta.env.VITE_BASE_URL}:5000`);
 
-const ModFleet = ({ data }) => {
+const Mod_Fleet = () => {
+    const [reqUrl, setReqUrl] = useState("")
     socket.on('connect', () => {
         console.log('Connected to the server');
     });
-    let dest = []
-    let coordinates
+    let dest = ["-33.8569", "150.2152"]
+    let coordinates = []
     let apiKey = "https://www.google.com/maps/embed/v1/directions?key=AIzaSyBrKuh2cITMMHsvbR4omhTFfTn6g024l9Y"
 
     socket.on('json', (incomingData) => {
         if (incomingData.sensorType == "GPS") {
-            coordinates = data.value.split("|")
-            dest = []
-            let reqUrl = apiKey + "&origin=" + coordinates[0] + "," + coordinates[1] + "&destination=" + dest[0] + "," + dest[1]
+            coordinates = incomingData.value.split("|")
+            let temp = apiKey + "&origin=" + coordinates[0] + "," + coordinates[1] + "&destination=" + dest[0] + "," + dest[1]
+            setReqUrl(temp)
+            console.log(reqUrl)
         }
-
     })
-
+    
     return (
         <>
             <iframe
                 width="450"
                 height="250"
-                frameBorder="0" style="border:0"
                 referrerPolicy="no-referrer-when-downgrade"
                 src={reqUrl}
-                allowfullscreen
             >
             </iframe>
         </>
     )
 }
 
-export default ModFleet
+export default Mod_Fleet
